b26bf175ceae133a9f6910d891ed64a681ef011d
Wooyanggang <didrod6589@naver.com>
2023-10-31 13:12:44 +0900

    배포테스트6

--- client/src/components/SmartBuy/SmartBuy.js
+++ client/src/components/SmartBuy/SmartBuy.js
@@ -1,13 +1,91 @@
 import React, { useState, useEffect } from 'react'
-import BuyStep from './BuyStep'
 import { useNavigate } from 'react-router-dom'
+import { Button, Steps, theme } from 'antd';
+import StepOne from './Steps/StepOne';
+import StepTwo from './Steps/StepTwo';
+import StepThree from './Steps/StepThree'
+
 
 const SmartBuy = () => {
-  return (
-    <div>
-      <BuyStep />
-    </div>
-  )
-}
+  const steps = [
+    {
+      title: '용도',
+      content: <StepOne />
+    },
+    {
+      title: '예산',
+      content: <StepTwo />
+    },
+    {
+      title: '추가구성',
+      content: <StepThree />,
+    },
+  ];
 
+
+  const navigate = useNavigate();
+  const { token } = theme.useToken();
+  const [current, setCurrent] = useState(0);
+  const next = () => {
+    setCurrent(current + 1);
+  };
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  const items = steps.map((item) => ({
+    key: item.title,
+    title: item.title,
+  }));
+  const contentStyle = {
+    position: 'relative',
+    width: '90%',
+    height: '60Vh',
+    textAlign: 'center',
+    color: token.colorTextTertiary,
+    backgroundColor: '#fff',
+    borderRadius: token.borderRadiusLG,
+    border: `1px dashed ${token.colorBorder}`,
+    margin: '10px 5%',
+  };
+  return (
+    <>
+      <Steps current={current} items={items} style={{ margin: '10px 5% 0', width: '90%' }} />
+      <div style={contentStyle}>{steps[current].content}</div>
+      <div
+        style={{
+          marginTop: 24,
+        }}
+      >
+        {current < steps.length - 1 && (
+          <Button type="primary"
+            style={{
+              margin: '0 5%',
+            }}
+            onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button type="primary"
+            style={{
+              margin: '0 5%',
+            }}
+            onClick={() => navigate('Result/')}>
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button
+            style={{
+              margin: '0 '
+            }}
+            onClick={() => prev()}
+          >
+            Previous
+          </Button>
+        )}
+      </div>
+    </>
+  );
+}
 export default SmartBuy
